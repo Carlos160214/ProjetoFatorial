36b75d623af25b0cfd2e8c3515f2c15eb0facdd9
Carlos Eduardo Botelho de Faria <carlos-faria@altamogiana.com.br>
2022-03-11 22:47:14 -0300

    Alteração correto para o calculo

--- fatorial.java
+++ fatorial.java
@@ -1,14 +1,19 @@
-public class Fatorial {
+public class fatorial {
    
 //versao interativa do calculo fatorial
  
-public  int calcularFatorial(int n) {
+public  int calcularFatorial(int n) throws IllegalArgumentException {
 
-int resultado = 1;
+if (n < 0)
+
+  throw new IllegalArgumentException("Não existe fatorial para numero negativos");
+
+int resultado= 1;
+if (n !=0){
 
 for (int i = 2; i <=n; i++)
   resultado*= i;
-
+}
 return resultado;
 
 }
