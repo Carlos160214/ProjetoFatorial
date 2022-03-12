89e4c37dad0e777e92fb585199271335ecc03ec7
Carlos Eduardo Botelho de Faria <carlos-faria@altamogiana.com.br>
2022-03-12 10:19:28 -0300

    Calculo de 10 e renomeacao da variavel

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,8 +1,8 @@
 class TesteFatorial {// teste
              public static void main(String[] args) {
                     fatorial fat = new fatorial();
-                    int resultado = fat.calcularFatorial(0);
-                    System.out.println (resultado);
+                    int r = fat.calcularFatorial(10);
+                    System.out.println (r);
 
 }
 
