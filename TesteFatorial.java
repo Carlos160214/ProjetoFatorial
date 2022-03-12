c4e96c9e093c8ebd885cf8b5927f905d2933555f
Carlos Eduardo Botelho de Faria <carlos-faria@altamogiana.com.br>
2022-03-12 10:18:08 -0300

    Calculo do fatorial em vez de zero

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,7 +1,7 @@
 class TesteFatorial {// teste
              public static void main(String[] args) {
                     fatorial fat = new fatorial();
-                    int resultado = fat.calcularFatorial(0);
+                    int resultado = fat.calcularFatorial(4);
                     System.out.println (resultado);
 
 }
